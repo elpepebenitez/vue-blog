79492769b55cfddb749074d51495b115ba9c9941
Jose Benitez <pepebenitez12@gmail.com>
2020-12-28 16:17:17 -0500

    Learned about using computed properties with the composition API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,31 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update Ninja One</button>
-
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update Ninja Two</button>
+    <input type="text" v-model="search">
+    <p>search term: {{search}}</p>
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
+    
 
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+import { ref, computed } from 'vue';
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   setup() {
-    const ninjaOne = ref({ name: 'Mario', age: 30})
-    const ninjaTwo = reactive({ name: 'Luigi', age: 35 })
-
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
-    }
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45
-    }
-
-    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo }
+    const search = ref('')
+    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+    
+    const matchingNames = computed(() => {
+        return names.value.filter((name) => name.includes(search.value))
+    } )
+    return { names, search, matchingNames }
   }
 }
 </script>
