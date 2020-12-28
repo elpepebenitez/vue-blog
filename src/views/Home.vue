776911fdfb046928c6854113599bfe3bab00b9a5
Jose Benitez <pepebenitez12@gmail.com>
2020-12-28 16:04:05 -0500

    Learned about refs vs reactive objects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p ref="p" >My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">Add 1 to age</button>
-    <input type="text" v-model="name">
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update Ninja One</button>
+
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update Ninja Two</button>
 
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   setup() {
-    console.log('setup');
+    const ninjaOne = ref({ name: 'Mario', age: 30})
+    const ninjaTwo = reactive({ name: 'Luigi', age: 35 })
 
-    const p = ref(null)
-
-    const name = ref('Mario')
-    const age = ref(30)
-
-    const handleClick = () => {
-      name.value = 'Luigi'
-      age.value = '28' 
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40
+    }
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 45
     }
 
-    return { name, age, handleClick, p }
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo }
   }
 }
 </script>
