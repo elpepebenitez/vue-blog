15030b61ff534e8e31afebdb9246b7230bf7a178
Jose Benitez <pepebenitez12@gmail.com>
2020-12-28 15:09:22 -0500

    Learned the basics of the composition API and the setup hook

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    console.log('setup');
+
+    let name = 'Pepe'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('you clicked me')
+    }
+
+    return { name, age, handleClick }
   }
 }
 </script>
